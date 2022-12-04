01807c6f68b6f728e035f3512b0ea4e5b407dc1d
LeeBJo <qudwh0312@naver.com>
2022-12-04 22:55:12 +0900

    detail update

--- frontend/src/routes/Detail.svelte
+++ frontend/src/routes/Detail.svelte
@@ -7,9 +7,10 @@
     import moment from 'moment/min/moment-with-locales'
     moment.locale('ko')
 
+
     export let params = {}
     let question_id = params.question_id
-    let question = {answers:[], voter:[], content: ''}
+    let question = {answers:[], voter:[], content:''}
     let content = ""
     let error = {detail:[]}
 
@@ -20,7 +21,8 @@
     }
 
     get_question()
-
+    
+    //답변 달기
     function post_answer(event) {
         event.preventDefault()
         let url = "/api/answer/create/" + question_id
@@ -39,6 +41,7 @@
         )
     }
 
+    //질문 삭제
     function delete_question(_question_id) {
         if(window.confirm('정말로 삭제하시겠습니까?')) {
             let url = "/api/question/delete"
@@ -56,6 +59,7 @@
         }
     }
 
+    //댓글 삭제
     function delete_answer(answer_id) {
         if(window.confirm('정말로 삭제하시겠습니까?')) {
             let url = "/api/answer/delete"
@@ -73,6 +77,7 @@
         }
     }
 
+    //추천버튼
     function vote_question(_question_id) {
         if(window.confirm('정말로 추천하시겠습니까?')) {
             let url = "/api/question/vote"
@@ -90,22 +95,6 @@
         }
     }
 
-    function vote_answer(answer_id) {
-        if(window.confirm('정말로 추천하시겠습니까?')) {
-            let url = "/api/answer/vote"
-            let params = {
-                answer_id: answer_id
-            }
-            fastapi('post', url, params, 
-                (json) => {
-                    get_question()
-                },
-                (err_json) => {
-                    error = err_json
-                }
-            )
-        }
-    }
 </script>
 
 <div class="container my-3">
@@ -134,7 +123,7 @@
                     추천
                     <span class="badge rounded-pill bg-success">{ question.voter.length }</span>
                 </button>
-                {#if question.user && $username === question.user.username }
+                {#if question.user && $username === question.user.username }  <!-- 질문 수정 링크는 로그인한 사용자와 글쓴이가 같은 경우에만 보임 -->
                 <a use:link href="/question-modify/{question.id}" 
                     class="btn btn-sm btn-outline-secondary">수정</a>
                 <button class="btn btn-sm btn-outline-secondary"
@@ -143,10 +132,6 @@
             </div>
         </div>
     </div>
-
-    <button class="btn btn-secondary" on:click="{() => {
-        push('/')
-    }}">목록으로</button>
 
     <!-- 답변 목록 -->
     <h5 class="border-bottom my-3 py-2">{question.answers.length}개의 답변이 있습니다.</h5>
@@ -184,15 +169,20 @@
         </div>
     </div>
     {/each}
+
+    <button class="btn btn-secondary" on:click="{() => {
+        push('/')
+    }}">목록으로</button>
+
     <!-- 답변 등록 -->
     <Error error={error} />
     <form method="post" class="my-3">
         <div class="mb-3">
             <textarea rows="10" bind:value={content} 
-                disabled={$is_login ? "" : "disabled"}
+                disabled={$is_login ? "" : "disabled"}      
                 class="form-control" />
         </div>
-        <input type="submit" value="답변등록" class="btn btn-primary {$is_login ? '' : 'disabled'}" 
+        <input type="submit" value="답변등록" class="btn btn-primary {$is_login ? '' : "disabled"}"
             on:click="{post_answer}" />
     </form>
 </div>
