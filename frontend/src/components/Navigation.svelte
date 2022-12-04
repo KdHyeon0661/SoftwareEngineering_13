d704de203746233aab818bfb266ef9f9eea0a3e2
LeeBJo <qudwh0312@naver.com>
2022-12-04 22:51:31 +0900

    네비, store update

--- frontend/src/components/Navigation.svelte
+++ frontend/src/components/Navigation.svelte
@@ -1,22 +1,22 @@
 <script>
-    import { link } from 'svelte-spa-router'
-    import { page, keyword, access_token, username, is_login } from "../lib/store"
+    import {link} from 'svelte-spa-router'
+    import {page, keyword, access_token, username, is_login, is_admin} from "../lib/store"
 </script>
 
 <!-- 네비게이션바 -->
 <nav class="navbar navbar-expand-lg navbar-light bg-light border-bottom">
     <div class="container-fluid">
-        <a use:link class="navbar-brand" href="/" 
-            on:click="{() => {$keyword = '', $page = 0}}">Pybo</a>
+        <a use:link class="navbar-brand" href="/"
+           on:click="{() => {$keyword = '', $page = 0}}">Pybo</a>
         <button
-            class="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent"
-            aria-expanded="false"
-            aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon" />
+                class="navbar-toggler"
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#navbarSupportedContent"
+                aria-controls="navbarSupportedContent"
+                aria-expanded="false"
+                aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"/>
         </button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
@@ -26,8 +26,20 @@
                             $access_token = ''
                             $username = ''
                             $is_login = false
+                            $is_admin = false
                         }}>로그아웃 ({$username})</a>
                     </li>
+                    <li class="nav-item">
+                        <a use:link class="nav-link" href="/recommend-diet">식단 추천</a>  ###
+                    </li>
+                    {#if $is_admin }
+                        <li class="nav-item">
+                            <a use:link class="nav-link" href="/health-info-list">건강정보</a> 
+                        </li>
+                        <li class="nav-item">
+                            <a use:link class="nav-link" href="/user-list">유저 조회</a> ###
+                        </li>
+                    {/if}
                 {:else}
                     <li class="nav-item">
                         <a use:link class="nav-link" href="/user-create">회원가입</a>
--- frontend/src/lib/store.js
+++ frontend/src/lib/store.js
@@ -9,8 +9,9 @@ const persist_storage = (key, initValue) => {
     return store
 }
 
-export const page = persist_storage("page", 0)
-export const keyword = persist_storage("keyword", "")
+export const page = persist_storage("page", 0)  
+export const keyword = persist_storage("keyword", 0)
 export const access_token = persist_storage("access_token", "")
 export const username = persist_storage("username", "")
 export const is_login = persist_storage("is_login", false)
+export const is_admin = persist_storage("is_admin", false)
